b23b6ae8c52ec6ef196a76bc28bb09bfe5249c38
eh.jin <lst5518@naver.com>
2026-09-23 18:56:06 +0900

    feat: Spring Boot 초기 프로젝트 설정

---
+++ first-grab/src/main/java/com/firstgrab/FirstGrabApplication.java
@@ -0,0 +1,13 @@
+package com.firstgrab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGrabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstGrabApplication.class, args);
+    }
+
+}
---
+++ first-grab/src/test/java/com/firstgrab/FirstGrabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstgrab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGrabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
