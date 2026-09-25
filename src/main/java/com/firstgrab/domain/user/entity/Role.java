7dd8f3da70c1fe29354b5b9eaa93b8395f1e3aec
eh.jin <lst5518@naver.com>
2026-09-26 04:03:51 +0900

    feat: User Entity, Repository 구현
    - User 엔티티 생성
    - Role enum 생성 (USER, ADMIN)
    - BaseEntity 생성 (createAt, updateAt)
    - UserRepository 생성

---
+++ src/main/java/com/firstgrab/domain/user/entity/Role.java
@@ -0,0 +1,6 @@
+package com.firstgrab.domain.user.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/firstgrab/domain/user/entity/User.java
@@ -0,0 +1,52 @@
+package com.firstgrab.domain.user.entity;
+
+import com.firstgrab.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private Role role;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    @Builder
+    private User(String email, String password, String name, Role role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+    }
+
+    public static User createUser(String email, String password, String name) {
+        return User.builder()
+                .email(email)
+                .password(password)
+                .name(name)
+                .role(Role.USER)
+                .build();
+    }
+}
